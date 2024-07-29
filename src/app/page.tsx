319a2773fca170aa959bf711434cac76b53d6bbf
Omer Mecitoglu <omer.mecitoglu@gmail.com>
2024-07-29 11:57:04 +0700

    update dependencies

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,11 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
 
 const Page = () => (
-  <h1>Hello World <FontAwesomeIcon icon={faHandPeace} /></h1>
+  <h1>
+    Hello World
+    {" "}
+    <FontAwesomeIcon icon={faHandPeace} />
+  </h1>
 );
 
 export default Page;
