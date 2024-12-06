95804439331b31ceceb266dbf041a63ddee703e8
Omer Mecitoglu <omer.mecitoglu@gmail.com>
2024-12-06 14:50:13 +0700

    build: update dependencies

--- eslint.config.js
+++ eslint.config.js
@@ -1,19 +1,9 @@
 import omer from "@omer-x/eslint-config";
-import nextPlugin from "@next/eslint-plugin-next";
 
 export default [
   ...omer,
   {
     rules: {
-    },
-  },
-  {
-    plugins: {
-      "@next/next": nextPlugin,
-    },
-    rules: {
-      ...nextPlugin.configs.recommended.rules,
-      ...nextPlugin.configs["core-web-vitals"].rules,
     },
   },
   {
